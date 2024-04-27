fc9f0b69295e63ed11185480cf94ed4f55269590
SamiG99 <cuppop0@gmail.com>
2024-04-27 20:09:38 +0200

    clearing complete rows

--- Tetris_Game_C_Sharp/Map.cs
+++ Tetris_Game_C_Sharp/Map.cs
@@ -13,8 +13,6 @@ namespace Tetris
 
 			get=> map[rows, columns];
 			set => map[rows, columns] = value;
-
-
         }
 
 
@@ -23,58 +21,83 @@ namespace Tetris
             Rows = r;
 			Columns = c;
 			map = new int[r , c];
-
-
         }
 
-//test
         public bool CheckIfIn(int rows, int columns)
         {
             bool withinRowBounds = rows >= 0 && rows < Rows;
             bool withinColumnBounds = columns >= 0 && columns < Columns;
-
             return withinRowBounds && withinColumnBounds;
         }
 
 
-        public bool EmptyMap(int rows, int columns)
+        public bool EmptyMap(int row, int columns)
         {
-
-
-            if (CheckIfIn(rows, columns))
-                return map[rows, columns] == 0;
+            if (CheckIfIn(row, columns))
+                return map[row, columns] == 0;
 
             else
                 return false;
         }
 
-        public bool FullRow(int rows)
+        public bool CheckFullRow(int row)
         {
             for(int i = 0; i < Columns; i++)
             {
-                if (map[rows,i] == 0)
+                if (map[row,i] == 0)
                 {
                     return false;
                 }
             }
             return true;
-
         }
 
-        public bool EmptyRows(int rows)
+        public bool CheckEmptyRow(int row)
         {
             for (int i = 0; i < Columns; i++)
             {
-                if (map[rows, i] != 0)
+                if (map[row, i] != 0)
                 {
                     return false;
                 }
             }
             return true;
-
         }
 
+        private void ClearRow(int row)
+        {
+            for (int i=0; i < Columns; i++)
+            {
+                map[row, i] = 0;
+            }
+        }
 
+        private void DropRow(int row, int numberOfRows)
+        {
+            for (int i = 0; i < Columns; i++)
+            {
+                map[row + numberOfRows, i] = map[row, i];
+                map[row, i] = 0;
+            }
+        }
+        
+        public int EmptyFinishedRows()
+        {
+            int clear = 0;
+            for (int row = Rows-1; row >= 0; row--)
+            {
+                if (CheckFullRow(row))
+                {
+                    ClearRow(row);
+                    clear ++;
+                }
+                else if (clear > 0)
+                {
+                    DropRow(row, clear);
+                }
+            }
+            return clear;
+        }
     }
 
 
